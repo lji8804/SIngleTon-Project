d35dec0d7c3bec2b0631792acb5756e1b5de181b
lji8804 <lji8804@gmail.com>
2021-10-20 13:02:06 +0900

    update1020

--- app/src/main/java/com/example/sns_project/UserInfo.java
+++ app/src/main/java/com/example/sns_project/UserInfo.java
@@ -1,13 +1,15 @@
 package com.example.sns_project;
 
 public class UserInfo {
+    private String Uid;
     private String name;
     private String phoneNumber;
     private String birthDay;
     private String address;
     private String photoUrl;
 
-    public UserInfo(String name, String phoneNumber, String birthDay, String address, String photoUrl){
+    public UserInfo(String Uid,String name, String phoneNumber, String birthDay, String address, String photoUrl){
+        this.Uid = Uid;
         this.name = name;
         this.phoneNumber = phoneNumber;
         this.birthDay = birthDay;
@@ -22,6 +24,12 @@ public class UserInfo {
         this.address = address;
     }
 
+    public String getUid() {
+        return Uid;
+    }
+    public void setUid(String uid) {
+        Uid = uid;
+    }
     public String getName(){
         return this.name;
     }
